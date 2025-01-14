ecb1d91b64fc6a1acec2ffe60c3da4dea43cc1bc
LB5JJ / Bård Bjerke Johannessen <cq@lb5jj.no>
2025-01-14 14:43:18 +0100

    Extracted the long "case"-es in main.py to function

--- TODO.md
+++ TODO.md
@@ -1,7 +1,1 @@
-- Separate out the longer cases in the main dispatch match with function calls!
 - Dont use "@enabled.setter" for setters with side effects!
-
-with open(args.file_base_name + ".cvs", "w") as csv:
-suffix is cvs
-not csv
-
--- main.py
+++ main.py
@@ -44,7 +44,9 @@ def main() -> None:
         args.serial_speed = int(env_serial_speed) if env_serial_speed else 115200
 
     with Load(args.serial_port, args.serial_speed) as load:
-        match args.command.lower():
+        command = args.command.lower()
+
+        match command:
             case "help":
                 help.main(args.argument)
 
@@ -61,130 +63,141 @@ def main() -> None:
                 load.enabled = False
 
             case "status":
-                state = "(On)" if load.enabled else "(Off)"
-
-                match load.mode:
-                    case Load.Mode.CV:
-                        print(f"Constant Voltage: {load.cv} V {state}")
-                    case Load.Mode.CC:
-                        print(f"Constant Current: {load.cc} A {state}")
-                    case Load.Mode.CR:
-                        print(f"Constant Resistance: {load.cr} Ω {state}")
-                    case Load.Mode.CP:
-                        print(f"Constant Power: {load.cp} W {state}")
-                
-                print(f"Voltage: {load.voltage} V")
-                print(f"Current: {load.current} A")
-                print(f"  Power: {load.power} W")
+                status(load)
             
-            case "constant-voltage":
-                if args.argument is None:
-                    help(command="constant-voltage", error="Missing argument")
-                load.cv = float(args.argument)
-                load.mode = Load.Mode.CV
-
-            case "constant-current":
-                if args.argument is None:
-                    help(command="constant-current", error="Missing argument")
-                load.cc = float(args.argument)
-                load.mode = Load.Mode.CC
-
-            case "constant-resistance":
-                if args.argument is None:
-                    help(command="constant-resistance", error="Missing argument")
-                load.cr = float(args.argument)
-                load.mode = Load.Mode.CR
-
-            case "constant-power":
-                if args.argument is None:
-                    help(command="constant-power", error="Missing argument")
-                load.cp = float(args.argument)
-                load.mode = Load.Mode.CP
+            case "constant-voltage" | "constant-current" | "constant-resistance" | "constant-power":
+                mode(load, command, args.argument)
 
             case "battery-test":
-                if args.constant_current and args.constant_power is None:
-                    load.cc = float(args.constant_current)
-                    load.mode = Load.Mode.CC
-                elif args.constant_power and args.constant_current is None:
-                    load.cp = float(args.constant_power)
-                    load.mode = Load.Mode.CP
-                else:
-                    help.main(command="battery-test", error="Must supply exactly one of --constant-current or --constant-power")
-
-                if args.cutoff_voltage is None and args.cutoff_seconds is None and not args.no_cutoff:
-                    help.main(command="battery-test", error="Must supply at least one of --cutoff-voltage, --cutoff-seconds or --no-cutoff")
-
-                with open(args.file_base_name + ".csv", "w") as csv:
-                    second = []
-                    volt = []
-
-                    coulombs = 0
-                    joules = 0
-
-                    csv.write("elapsed,voltage,current,power\n")
-
-                    start = int(time.time())
-
-                    load.enabled = True
-
-                    ampere_hours = 0
-                    watt_hours = 0
-
-                    while not stop:
-                        runtime = int(time.time()) - start
-
-                        voltage = load.voltage
-                        current = load.current
-                        power = load.power
-
-                        second.append(runtime)
-                        volt.append(voltage)
-
-                        coulombs += current * args.sampling_interval
-                        joules += power * args.sampling_interval
-
-                        ampere_hours = coulombs / 3600
-                        watt_hours = joules / 3600
-
-                        if args.verbose:
-                            print(f"{runtime} s: {voltage} V, {current} A, {power} W, {ampere_hours:.2f} Ah, {watt_hours:.2f} Wh    ", end="\r")
-
-                        csv.write(f"{runtime},{voltage},{current},{power}\n")
-        
-                        if args.cutoff_voltage and args.cutoff_voltage > voltage:
-                            break
-
-                        if args.cutoff_seconds and runtime > args.cutoff_seconds:
-                            break
-
-                        time.sleep(args.sampling_interval)
-
-                    load.enabled = False
-
-                    figur, axis = plt.subplots()
-
-                    title_mode = f"Constant {'Current' if args.constant_current else 'Power'}"
-                    title_rate = f"{args.constant_current} A" if args.constant_current else f"{args.constant_power} W"
-                    title_ampere_hours = f"{ampere_hours:.2f} Ah" if ampere_hours >= 1 else f"{int(ampere_hours*1000)} mAh"
-                    title_watt_hours = f"{watt_hours:.2f} Wh" if watt_hours >= 1 else f"{int(watt_hours*1000)} mWh"
-
-                    axis.set_ylabel("Volt")
-                    axis.set_xlabel("Seconds")
-                    axis.set_title(f"{title_mode} {title_rate}: {title_ampere_hours}, {title_watt_hours}")
-
-                    axis.plot(second, volt)
-
-                    plt.grid(linestyle=":")
-
-                    plt.savefig(args.file_base_name + ".png")
-
-                    if args.verbose:
-                        print("")
+                battery_test(load, args)
 
             case _:
                 help.main()
 
     exit(0)
+
+def mode(load, mode, arg):
+    if arg is None:
+        help(command=mode, error="Missing argument")
+
+    match mode:
+        case "constant-voltage":
+            load.cv = float(args.argument)
+            load.mode = Load.Mode.CV
+
+        case "constant-current":
+            load.cc = float(args.argument)
+            load.mode = Load.Mode.CC
+
+        case "constant-resistance":
+            load.cr = float(args.argument)
+            load.mode = Load.Mode.CR
+
+        case "constant-power":
+            load.cp = float(args.argument)
+            load.mode = Load.Mode.CP
+
+
+def status(load):
+    state = "(On)" if load.enabled else "(Off)"
+
+    match load.mode:
+        case Load.Mode.CV:
+            print(f"Constant Voltage: {load.cv} V {state}")
+        case Load.Mode.CC:
+            print(f"Constant Current: {load.cc} A {state}")
+        case Load.Mode.CR:
+            print(f"Constant Resistance: {load.cr} Ω {state}")
+        case Load.Mode.CP:
+            print(f"Constant Power: {load.cp} W {state}")
+                
+    print(f"Voltage: {load.voltage} V")
+    print(f"Current: {load.current} A")
+    print(f"  Power: {load.power} W")
+
+
+def battery_test(load, args):
+    if args.constant_current and args.constant_power is None:
+        load.cc = float(args.constant_current)
+        load.mode = Load.Mode.CC
+    elif args.constant_power and args.constant_current is None:
+        load.cp = float(args.constant_power)
+        load.mode = Load.Mode.CP
+    else:
+        help.main(command="battery-test", error="Must supply exactly one of --constant-current or --constant-power")
+
+    if args.cutoff_voltage is None and args.cutoff_seconds is None and not args.no_cutoff:
+        help.main(command="battery-test", error="Must supply at least one of --cutoff-voltage, --cutoff-seconds or --no-cutoff")
+
+    print(args)
+    
+    with open(args.file_base_name + ".csv", "w") as csv:
+        second = []
+        volt = []
+
+        coulombs = 0
+        joules = 0
+
+        csv.write("elapsed,voltage,current,power\n")
+
+        start = int(time.time())
+
+        load.enabled = True
+
+        ampere_hours = 0
+        watt_hours = 0
+
+        while not stop:
+            runtime = int(time.time()) - start
+
+            voltage = load.voltage
+            current = load.current
+            power = load.power
+
+            second.append(runtime)
+            volt.append(voltage)
+
+            coulombs += current * args.sampling_interval
+            joules += power * args.sampling_interval
+
+            ampere_hours = coulombs / 3600
+            watt_hours = joules / 3600
+
+            if args.verbose:
+                print(f"{runtime} s: {voltage} V, {current} A, {power} W, {ampere_hours:.2f} Ah, {watt_hours:.2f} Wh    ", end="\r")
+
+            csv.write(f"{runtime},{voltage},{current},{power}\n")
+        
+            if args.cutoff_voltage and args.cutoff_voltage > voltage:
+                break
+
+            if args.cutoff_seconds and runtime > args.cutoff_seconds:
+                break
+
+            time.sleep(args.sampling_interval)
+
+        load.enabled = False
+
+        figur, axis = plt.subplots()
+
+        title_mode = f"Constant {'Current' if args.constant_current else 'Power'}"
+        title_rate = f"{args.constant_current} A" if args.constant_current else f"{args.constant_power} W"
+        title_ampere_hours = f"{ampere_hours:.2f} Ah" if ampere_hours >= 1 else f"{int(ampere_hours*1000)} mAh"
+        title_watt_hours = f"{watt_hours:.2f} Wh" if watt_hours >= 1 else f"{int(watt_hours*1000)} mWh"
+
+        axis.set_ylabel("Volt")
+        axis.set_xlabel("Seconds")
+        axis.set_title(f"{title_mode} {title_rate}: {title_ampere_hours}, {title_watt_hours}")
+
+        axis.plot(second, volt)
+
+        plt.grid(linestyle=":")
+
+        plt.savefig(args.file_base_name + ".png")
+
+        if args.verbose:
+            print("")
+
 
 if __name__ == '__main__':
     main()
